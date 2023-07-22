15d1c4b5946901d6b21b69f222c050d3aa4386fa
rdwz <8125196+rdwz@users.noreply.github.com>
2023-07-22 16:09:07 +0200

    7 files modified, 1 added, 1 deleted

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import compress from "astro-compress";
+import compressor from "astro-compressor";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "astro-sitemap";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [compress(), compressor(), robotsTxt(), sitemap()],
+  site: "https://astro.build"
+});
